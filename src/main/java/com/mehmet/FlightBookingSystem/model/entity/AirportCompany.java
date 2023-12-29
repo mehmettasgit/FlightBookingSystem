1f4717985bd36895cb13b5fdb28a66cf146b32a7
mehmet-eee <mehmet.tsgt@gmail.com>
2023-12-30 00:21:39 +0300

    AirportDelete was updated

--- src/main/java/com/mehmet/FlightBookingSystem/model/entity/AirportCompany.java
+++ src/main/java/com/mehmet/FlightBookingSystem/model/entity/AirportCompany.java
@@ -1,4 +1,7 @@
 package com.mehmet.FlightBookingSystem.model.entity;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.mehmet.FlightBookingSystem")
 public class AirportCompany {
 }
